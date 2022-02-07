bae27de369905c18266eb105d83cfc310431727e
Thinker92 <vilsonapo@gmail.com>
2022-02-07 12:40:51 -0500

    Added more questions and a choice list for licensing

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const questions = () =>
   inquirer.prompt([
     {
       type: "input",
+      name: "author",
+      message: "What is the author's name?",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
+    {
+      type: "input",
       name: "title",
       message: "What is the title of your project?",
     },
@@ -17,6 +32,13 @@ const questions = () =>
       name: "description",
       message: "Please give a brief description of your project:",
     },
+    {
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+
     {
       type: "input",
       name: "installation",
