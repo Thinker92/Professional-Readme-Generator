7febab6242c65dd3ff5974f99516bf283fdc3992
Thinker92 <vilsonapo@gmail.com>
2022-02-22 17:41:12 -0500

    Testing Filesystem methods

--- index.js
+++ index.js
@@ -3,68 +3,79 @@ const fs = require("fs");
 const util = require("util");
 
 const questions = () =>
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "author",
-      message: "What is the author's name?",
-    },
-    {
-      type: "input",
-      name: "username",
-      message: "What is your GitHub username?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email address?",
-    },
-    {
-      type: "input",
-      name: "title",
-      message: "What is the title of your project?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Please give a brief description of your project:",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "What kind of license should your project have?",
-      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
-    },
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "author",
+        message: "What is the author's name?",
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please give a brief description of your project:",
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+      },
 
-    {
-      type: "input",
-      name: "installation",
-      message:
-        "Provide what command should be used to install dependancies and any additonal installation instructions:",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "What command should be run to run tests?",
-    },
+      {
+        type: "input",
+        name: "installation",
+        message:
+          "Provide what command should be used to install dependancies and any additonal installation instructions:",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?",
+      },
 
-    {
-      type: "input",
-      name: "usage",
-      message: "Provide usage information and/or examples for use:",
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message:
-        "Enter instructions for future developers to contribute to your project:",
-    },
-    {
-      type: "input",
-      name: "githubURL",
-      message: "Provide a link to the GitHub Repository:",
-    },
-  ]);
+      {
+        type: "input",
+        name: "usage",
+        message: "Provide usage information and/or examples for use:",
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message:
+          "Enter instructions for future developers to contribute to your project:",
+      },
+      {
+        type: "input",
+        name: "githubURL",
+        message: "Provide a link to the GitHub Repository:",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      fs.writeFile(
+        answers.title + ".json",
+        JSON.stringify(answers, null, "\t"),
+        (err) => {
+          err ? console.error(err) : console.log("Success!");
+        }
+      );
+    });
 
 // TODO: Create a function to write README file
 const writeToFile = util.promisify(fs.writeFile);
